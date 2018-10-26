0cd181649112c78e2e9b2b1fb471c66e79215d2e
John Guggenbiller <42472160+johnguggy@users.noreply.github.com>
2018-10-26 14:23:05 -0400

    Add files via upload

---
+++ Loops/Loop1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            double num1, num2, prod = 0;
+            int counter;
+            for (counter = 0; counter <= 9; counter++)
+            {
+                num1 = rnd.Next(0, 9);
+                num2 = rnd.Next(0, 9);
+                prod = num1 * num2;
+                Console.WriteLine("The product of " + num1 + "*" + num2 + " = " + prod);
+                Console.WriteLine();
+            }
+        }
+    }
+}
